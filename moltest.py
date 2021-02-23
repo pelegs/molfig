d687c6e1c76b203f19b393f8aa84775f05b5f01f
Peleg Sapir <peleg@ds.mpg.de>
2021-02-23 13:40:02 +0100

    All relevant data from OpenBabel in files

--- moltest.py
+++ moltest.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: iso-8859-15 -*-
 
-import openbabel
+from openbabel import openbabel
 from openbabel import pybel
 
-mol = pybel.readstring('smi', 'CCO')
+
+SMILES = 'CC(=O)C'
+
+
+mol = pybel.readstring('smi', SMILES)
 mol.addh()
 mol.make2D()
-for atom in mol.atoms:
-    print(atom.atomicnum, atom.coords)
+for i, atom in enumerate(mol.atoms):
+    print(i+1, atom.atomicnum, atom.coords)
+
+obConversion = openbabel.OBConversion()
+obConversion.SetInAndOutFormats('smi', 'mdl')
+mol = openbabel.OBMol()
+obConversion.ReadString(mol, SMILES)
+mol.AddHydrogens()
+num_bonds = mol.NumBonds()
+for i in range(num_bonds):
+    bond = mol.GetBond(i)
+    print(i+1, bond.GetBondOrder(), bond.GetBeginAtomIdx(), bond.GetEndAtomIdx())
